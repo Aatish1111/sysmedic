b6504ab3a48f3b4c61aff7ef64fa9f64d5b5b127
Aatish1111 <aatishkumarmoharana@gmail.com>
2022-08-25 16:39:45 +0530

    Authentication commit

--- frontend/sysmedicweb/src/App.js
+++ frontend/sysmedicweb/src/App.js
@@ -1,13 +1,20 @@
-import React from "react"
-import './App.css';
-import Main from "./main/Main"
+import { Route, Routes, Navigate } from "react-router-dom";
+import Main from "./components/Main";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
-  return (
-    <div className="App">
-       <Main />
-    </div>
-  );
+	const user = localStorage.getItem("token");
+	console.log(user)
+
+	return (
+		<Routes>
+			{user && <Route path="/" exact element={<Main />} />}
+			<Route path="/signup" exact element={<Signup />} />
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+		</Routes>
+	);
 }
 
 export default App;
